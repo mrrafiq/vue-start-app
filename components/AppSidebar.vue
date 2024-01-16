ed470e7cb80ca044e75a4d15539ea2e6548bfe91
mrrafiq <rafiqmp3@gmail.com>
2024-01-16 10:48:39 +0700

    initial commit

---
+++ components/AppSidebar.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <v-navigation-drawer
+    v-model="drawer"
+    :rail="rail"
+    permanent
+    
+    >
+      <v-list>
+        <v-list-item
+          prepend-avatar="https://randomuser.me/api/portraits/women/85.jpg"
+          title="Sandra Adams"
+          subtitle="sandra_a88@gmailcom"
+        ></v-list-item>
+      </v-list>
+
+      <v-divider></v-divider>
+
+      <v-list density="compact" nav>
+        <v-list-item prepend-icon="mdi-folder" title="My Files" value="myfiles"></v-list-item>
+        <v-list-item prepend-icon="mdi-account-multiple" title="Shared with me" value="shared"></v-list-item>
+        <v-list-item prepend-icon="mdi-star" title="Starred" value="starred"></v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+
+    <v-app-bar>
+      <v-app-bar-nav-icon
+        @click="rail = !rail"
+      >
+      </v-app-bar-nav-icon>
+      <v-app-bar-title>My Application</v-app-bar-title>
+    </v-app-bar>
+  </div>
+  
+  
+</template>
+<script setup lang="ts">
+  var rail = ref(true)
+  var drawer = ref(true)
+</script>
---
+++ layouts/default.vue
@@ -0,0 +1,13 @@
+
+<template>
+  <v-app>
+    <v-card>
+      <v-layout>
+        <AppSidebar />
+        <v-main>
+          <slot />
+        </v-main>
+      </v-layout>
+    </v-card>
+  </v-app>
+</template>
